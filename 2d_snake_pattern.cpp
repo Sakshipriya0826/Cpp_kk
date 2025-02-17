23547048aacc836a43280e87bc63c288ce7ee9ad
Sakshipriya0826 <priyasakshi745@gmail.com>
2025-02-17 07:24:36 +0530

    2d_snake_pattern.cpp

---
+++ 2d_snake_pattern.cpp
@@ -0,0 +1,24 @@
+//Print matrix in snake pattern
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int row=sizeof(arr)/sizeof(arr[0]);
+    int col=sizeof(arr[0])/sizeof(arr[0][0]);
+
+    for(int i=0;i<row;i++){
+            if(i%2==0){
+                for(int j=0;j<col;j++){
+                    cout<<arr[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            else{
+                for(int j=0;j<col;j++){
+                    cout<<arr[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+        }
+}
